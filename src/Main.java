78dc90085e7851f48de842b5eb475a024c84e177
w-kon <wkon@yandex.ru>
2024-08-11 18:50:04 +0600

    Initil commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price =18940;
+        int one_mile = 20;
+        int bonus_miles;
+        bonus_miles = ticket_price/one_mile;
+        System.out.println(bonus_miles);
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
